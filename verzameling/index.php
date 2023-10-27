2868b9b9d3aafd09d0c729eddd197459a3d9fa1d
Steven <120721896+StevenOtterspeer@users.noreply.github.com>
2023-10-27 09:29:08 +0200

    nog verzameling + bestel functie

--- verzameling/index.php
+++ verzameling/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/CSS/nav.css">
-    <link rel="stylesheet" href="/CSS/verzameling.css">
+    <link rel="stylesheet" href="../CSS/nav.css">
+    <link rel="stylesheet" href="../CSS/verzameling.css">
     <title>Verzameling</title>
 </head>
 <body>
@@ -13,10 +13,10 @@
         <div class="navbar">
           <ul>
             <a href="../images/hotwheelsLogo.png" class="logo">Logo</a>
-            <li><a href="">Admin</a></li>
-            <li><a href="">Betsellen</a></li>
-            <li><a href="">Verzameling</a></li>
-            <li><a class="active" href="">Home</a></li>
+            <li><a href="../about/index.html">About</a></li>
+            <li><a href="../contact/index.html">Contact</a></li>
+            <li><a class="active" href="../verzamelling/index.php">Verzameling</a></li>
+            <li><a href="../home/index.html">Home</a></li>
           </ul>
         </div>
       </nav>
